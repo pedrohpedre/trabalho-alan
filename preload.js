6d2b7320b47e61767f7d23ab6181ff6b711d62a2
pedrohpedre <pedro.pedrezini@edu.unipar.br>
2025-04-23 19:19:05 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  enviarPergunta: (pergunta) => ipcRenderer.invoke('enviar-pergunta', pergunta)
-});
